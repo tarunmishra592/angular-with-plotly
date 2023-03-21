804c25d02bb01b1a6d316bec4541916c744be868
Tarun <tarun.mishra592@gmail.com>
2023-03-22 00:12:38 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'stockvizui-plotly-ssr';
+  title = 'angular-with-plotly';
   
 }
